c9ed0519f614324467f6851efa8f1d92bd2dcca7
Nicolas <nicolaslanusse4@gmail.com>
2023-04-16 13:17:13 +0200

    add migration for centro.js

--- models/centro.js
+++ models/centro.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
 
-      Centro.hasMany(models.Cita, {
+      Centro.belongsTo(models.Cita, {
         foreignKey: "id_centro", // foreignKey en modelo Cita
       });
     }
