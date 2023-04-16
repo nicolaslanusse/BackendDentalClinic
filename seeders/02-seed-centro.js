da86bfb23a46ea6ded9b097856601de09ea27070
Nicolas <nicolaslanusse4@gmail.com>
2023-04-16 15:18:29 +0200

    add seed for usuarios

--- seeders/02-seed-centro.js
+++ seeders/02-seed-centro.js
@@ -1,28 +1,34 @@
 "use strict";
 
+const { Op } = require("sequelize");
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert("centros", [
-      {
-        nombre_lugar: "Clinica Barcelona",
-        direccion: "Gran via 576",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        nombre_lugar: "Clinica Madrid",
-        direccion: "C. de Juan de Mariana 15",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        nombre_lugar: "Clinica Valencia",
-        direccion: "Av. de Catalunya 11",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ],{});
+    await queryInterface.bulkInsert(
+      "centros",
+      [
+        {
+          nombre_lugar: "Clinica Barcelona",
+          direccion: "Gran via 576",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre_lugar: "Clinica Madrid",
+          direccion: "C. de Juan de Mariana 15",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre_lugar: "Clinica Valencia",
+          direccion: "Av. de Catalunya 11",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
     /**
      * Add seed commands here.
      *
--- seeders/03-seed-usuario.js
+++ seeders/03-seed-usuario.js
@@ -1,8 +1,49 @@
-'use strict';
+"use strict";
 
+const { Op } = require("sequelize");
+const bcrypt = require("bcrypt");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "centros",
+      [
+        {
+          nombre: "Luis",
+          apellidos: "Suarez",
+          edad: 19,
+          email: "bolso@gmail.com",
+          telefono: 616540798,
+          password: bcrypt.hashSync("bolso", 10),
+          id_rol: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre: "Francisco",
+          apellidos: "Diaz",
+          edad: 29,
+          email: "pancho@gmail.com",
+          telefono: 632989051,
+          password: bcrypt.hashSync("admin", 10),
+          id_rol: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          nombre: "Alberto",
+          apellidos: "Mendez",
+          edad: 45,
+          email: "alberto@gmail.com",
+          telefono: 632872539,
+          password: bcrypt.hashSync("alberto", 10),
+          id_rol: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
     /**
      * Add seed commands here.
      *
@@ -11,15 +52,29 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
+
+    await queryInterface.bulkDelete("centros", {
+      [Op.or]: [
+        {
+          nombre: "Luis",
+        },
+        {
+          nombre: "Francisco",
+        },
+        {
+          nombre: "Alberto",
+        },
+      ],
+    });
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
