aafa0664835d89a13b341a10e66b271101976eac
Nicolas <nicolaslanusse4@gmail.com>
2023-04-16 15:25:58 +0200

    add seed for pacientes

--- seeders/05-seed-paciente.js
+++ seeders/05-seed-paciente.js
@@ -1,8 +1,19 @@
-'use strict';
-
+"use strict";
+const { Op } = require("sequelize");
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "pacientes",
+      [
+        {
+          id_usuario: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
     /**
      * Add seed commands here.
      *
@@ -11,15 +22,18 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("doctores", {
+      [Op.or]: [{ id_usuario: 1 }],
+    });
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
