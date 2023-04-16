7d2df514905b863b4c4a9a9d1e6b9aad7870310a
Nicolas <nicolaslanusse4@gmail.com>
2023-04-16 13:56:04 +0200

    add seeders

---
+++ seeders/01-seed-rol.js
@@ -0,0 +1,32 @@
+"use strict";
+const { Op } = require("sequelize");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("roles", [
+      { nombre_rol: "paciente", createdAt: new Date(), updatedAt: new Date() },
+      { nombre_rol: "doctor", createdAt: new Date(), updatedAt: new Date() },
+      { nombre_rol: "admin", createdAt: new Date(), updatedAt: new Date() },
+    ]);
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("roles", {
+      [Op.or]: [
+        { nombre_rol: "paciente" },
+        { nombre_rol: "doctor" },
+        { nombre_rol: "admin" },
+      ],
+    });
+  },
+};
