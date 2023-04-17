2ec6907f608dccac12d381b0121d93ce4404ec43
Nicolas <nicolaslanusse4@gmail.com>
2023-04-17 18:30:58 +0200

    add folder of utilities

---
+++ _util/hash.js
@@ -0,0 +1,8 @@
+const bcrypt = require("bcrypt");
+const hash = (myPlaintextPassword) => bcrypt.hashSync(myPlaintextPassword, 10);
+const compareHash = (myPlaintextPassword, hash) =>
+  bcrypt.compareSync(myPlaintextPassword, hash);
+module.exports = {
+  hash,
+  compareHash,
+};
