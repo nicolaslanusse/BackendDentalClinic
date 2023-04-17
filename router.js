87f9e8a140dbd761a7bc63d1e83a9bee2bc6f8af
Nicolas <nicolaslanusse4@gmail.com>
2023-04-17 18:30:16 +0200

    update router

--- router.js
+++ router.js
@@ -2,10 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 const indexRouter = require("./routes/index");
-
+const authRouter = require("./routes/auth");
+// const userRouter = require("./routes/user");
+// const appointmentRouter = require("./routes/appointment");
 
 /* home page */
 router.use("/", indexRouter);
 
+/* authentication */
+router.use("/auth", authRouter);
+// /* users */
+// router.use("/register", userRouter)
+// /* appointment */
+// router.use("appointment", appointmentRouter)
 
 module.exports = router;
