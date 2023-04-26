43e15c2db187da08d1bceb125879dafdab76e820
Nicolas <nicolaslanusse4@gmail.com>
2023-04-26 16:06:03 +0200

    update and fix error for migrations

--- migrations/04-create-doctor.js
+++ migrations/04-create-doctor.js
@@ -31,6 +31,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Doctores");
+    await queryInterface.dropTable("doctores");
   },
 };
