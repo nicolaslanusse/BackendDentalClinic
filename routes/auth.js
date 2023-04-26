7e20271eae47221bd50789b98ff03bc9e68ede7f
Nicolas <nicolaslanusse4@gmail.com>
2023-04-26 16:05:09 +0200

    add new routes for auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,16 @@
 const authController = require("../controllers/authController");
-
+const isAdmin = require("../middlewares/isAdmin");
+const verifyToken = require("../middlewares/verifyToken");
 const router = require("express").Router();
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
+
+router.post(
+  "/register/doctor",
+  verifyToken,
+  isAdmin,
+  authController.registerDoctor
+);
 
 module.exports = router;
